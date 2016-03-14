2b31ae69954fab7c4defbf1ccc0546dbb02487c9
Christopher Stein <cstein@bmcc.cuny.edu>
2016-03-14 06:00:56 -0400

    minor formatting issue fix

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
                     'depth'             => 2,
                     'container'         => 'div',
                     'container_class'   => 'collapse navbar-collapse',
-            'container_id'      => 'bs-example-navbar-collapse-1',
+                    'container_id'      => 'bs-example-navbar-collapse-1',
                     'menu_class'        => 'nav navbar-nav',
                     'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
                     'walker'            => new wp_bootstrap_navwalker())
